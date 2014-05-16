6c6b24ca2b22623cd5c3138d59cf2fd7578f533e
simurai <noreply>
2014-05-16 03:28:58 +0000

    Update components

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -14,4 +14,29 @@ exports.Main = Component.specialize(/** @lends Main# */ {
             this.super();
         }
     }
+    
+    _color: {
+        value: 0
+    },
+                                    
+    color: {
+        set: function(value) {
+            this._color = value;
+            this.needsDraw = true;
+        },
+        get: function() {
+            return this._color;
+        }
+    },
+
+    draw: {
+        value: function() {
+            var hue = Math.round(this._color);
+            var color = "hsl(" + hue + ",80%,60%)";
+
+            this.style.color = color;
+            this.style.fill = color;
+        }
+    }
+        
 });
