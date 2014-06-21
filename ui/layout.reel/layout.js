a2a72349bfb8882c5df06a47e8e4da9ae69a2ad8
simurai <simulus@gmail.com>
2014-06-21 16:37:07 +0900

    Add layout section

---
+++ ui/layout.reel/layout.js
@@ -0,0 +1,11 @@
+
+var Component = require("montage/ui/component").Component;
+
+exports.Layout = Component.specialize( {
+    constructor: {
+        value: function Layout() {
+            this.super();
+        }
+    }
+        
+});
