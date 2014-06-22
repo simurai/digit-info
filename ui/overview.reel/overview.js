5a0fb5c0953d171678e59c69e2040832fc74114a
simurai <simulus@gmail.com>
2014-06-22 11:18:41 +0900

    Add Overview

---
+++ ui/overview.reel/overview.js
@@ -0,0 +1,11 @@
+
+var Component = require("montage/ui/component").Component;
+
+exports.Overview = Component.specialize( {
+    constructor: {
+        value: function Overview() {
+            this.super();
+        }
+    }
+        
+});
