37dee4d368d191f4ce8e727cc5263750ed6241ef
simurai <simulus@gmail.com>
2014-06-27 19:55:13 +0900

    Add custom.reel

---
+++ ui/custom.reel/custom.js
@@ -0,0 +1,11 @@
+
+var Component = require("montage/ui/component").Component;
+
+exports.Custom = Component.specialize( {
+    constructor: {
+        value: function Custom() {
+            this.super();
+        }
+    }
+        
+});
